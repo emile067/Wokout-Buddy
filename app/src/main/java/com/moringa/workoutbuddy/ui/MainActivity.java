a9281e2699f8ab09c0a0fe9fc3500ec7e2074652
emile kamana <emileizere@gmail.com>
2021-02-03 17:17:14 +0200

    create new workout page

--- app/src/main/java/com/moringa/workoutbuddy/ui/MainActivity.java
+++ app/src/main/java/com/moringa/workoutbuddy/ui/MainActivity.java
@@ -2,15 +2,32 @@ package com.moringa.workoutbuddy.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.moringa.workoutbuddy.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.startButton)
+    Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewWorkoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
