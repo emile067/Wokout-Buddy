9968ad5b954321868679f20fef8737c943101bd4
emile kamana <emileizere@gmail.com>
2021-02-04 04:45:49 +0200

    create view workout and my workouts activities

---
+++ app/src/main/java/com/moringa/workoutbuddy/ui/ViewWorkoutActivity.java
@@ -0,0 +1,16 @@
+package com.moringa.workoutbuddy.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringa.workoutbuddy.R;
+
+public class ViewWorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_workout);
+    }
+}
---
+++ app/src/main/java/com/moringa/workoutbuddy/ui/WorkoutListActivity.java
@@ -0,0 +1,16 @@
+package com.moringa.workoutbuddy.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringa.workoutbuddy.R;
+
+public class WorkoutListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_list);
+    }
+}
