0a8d54098eea2c4732661c01f5b3315d4ca291e4
emile kamana <emileizere@gmail.com>
2021-02-04 17:10:29 +0200

    create login and create account layouts

---
+++ app/src/main/java/com/moringa/workoutbuddy/Constants.java
@@ -0,0 +1,5 @@
+package com.moringa.workoutbuddy;
+
+public class Constants {
+    public final static String FIREBASE_CHILD_WORKOUTS = "workouts";
+}
---
+++ app/src/main/java/com/moringa/workoutbuddy/ui/NewAccountActivity.java
@@ -0,0 +1,16 @@
+package com.moringa.workoutbuddy.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringa.workoutbuddy.R;
+
+public class NewAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
+}
